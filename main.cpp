004e28d688f4fe8c2eb9f4f31f8bef8cdec53e4c
Matzsche <66742471+Mack68426@users.noreply.github.com>
2023-05-20 00:44:36 +0800

    Change Tab to Spaces

--- main.cpp
+++ main.cpp
@@ -11,23 +11,23 @@ struct is_sorted
 template<int Start, int Head, int... Other>
 struct is_sorted<Start, Head, Other...>
 :std::integral_constant<bool, 
-						(Start <= Head) &&
-						is_sorted<Head, Other...>::value>
+                        (Start <= Head) &&
+                        is_sorted<Head, Other...>::value>
 
 {
 };
 
 int main(int argc, char* argv[])
 {
-	std::cout << std::boolalpha 
-			  << is_sorted<1, 4, 5, 9, 12>::value << std::endl;
+    std::cout << std::boolalpha 
+              << is_sorted<1, 4, 5, 9, 12>::value << std::endl;
 
-	std::cout << std::boolalpha
-			  << is_sorted<1, 1, 3, 4, 5>::value << std::endl;
+    std::cout << std::boolalpha
+              << is_sorted<1, 1, 3, 4, 5>::value << std::endl;
 
-	std::cout << std::boolalpha 
-			  << is_sorted<1, 2, 4, 3, 5>::value << std::endl;
+    std::cout << std::boolalpha 
+              << is_sorted<1, 2, 4, 3, 5>::value << std::endl;
 
-	std::cout << std::boolalpha
-			  << is_sorted<>::value << std::endl;
+    std::cout << std::boolalpha
+              << is_sorted<>::value << std::endl;
 }
